da8fe5921bfad0d003141547052837db0c8c0e13
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-03-05 13:55:40 +0300

    Перед установкой cross-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js'
         },
 module: {
     rules: [{ 
         test: /\.js$/, 
         use: { loader: "babel-loader" }, 
         exclude: /node_modules/ 
+            },
+            {
+                test: /\.css$/, 
+                use: [MiniCssExtractPlugin.loader, 'css-loader'] 
+            },
+            {
+                test: /\.(png|jpg|gif|ico|svg)$/,
+                use: [
+                    'file-loader?name=./images/[name].[ext]',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true
+                        }
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({ // настроили плагин
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new WebpackMd5Hash()
+    ]
 };
