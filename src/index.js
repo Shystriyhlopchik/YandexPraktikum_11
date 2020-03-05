842b97a2f8dd5106aa87d74c45bb8a245c669ab9
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-03-05 21:13:52 +0300

    Небольшие поправки

---
+++ src/index.js
@@ -0,0 +1,12 @@
+    // <script src="scripts/classes/Api.js"></script>
+    // <script src="scripts/classes/NewElement.js"></script>
+    // <script src="scripts/classes/Card.js"></script>
+    // <script src="scripts/classes/CardList.js"></script>
+    // <script src="scripts/classes/Popup.js"></script>
+    // <script src="scripts/classes/PopupForm.js"></script>
+    // <script src="scripts/classes/PopupImage.js"></script>
+    // <script src="scripts/classes/Validation.js"></script>
+    // <script src="scripts/classes/UserInfo.js"></script>
+    // <script src="scripts/index.js"></script> 
+
+    // imort 
