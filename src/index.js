e86a18151bbe1e9c848c398cdba2a8e25f34cdbe
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-03-06 16:16:32 +0300

    add Api modul

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
     import './style.css';
-    // <script src="scripts/classes/Api.js"></script>
-    // <script src="scripts/classes/NewElement.js"></script>
-    // <script src="scripts/classes/Card.js"></script>
-    // <script src="scripts/classes/CardList.js"></script>
-    // <script src="scripts/classes/Popup.js"></script>
-    // <script src="scripts/classes/PopupForm.js"></script>
-    // <script src="scripts/classes/PopupImage.js"></script>
-    // <script src="scripts/classes/Validation.js"></script>
-    // <script src="scripts/classes/UserInfo.js"></script>
-    // <script src="scripts/index.js"></script> 
 
-    // imort 
+    import Api from './scripts/api.js';
+
+
+    // ---------------------------------- Свойства -----------------------------------------------------  //
+    // Окно с формой новой карточки
+    const newCardPopupProps = {
+        id: 'new-card',
+        title: 'Новое место',
+        formName: 'new',
+        firstFormFieldType: 'text',
+        firstFormFieldName: 'name',
+        secondFieldType: 'url',
+        secondFieldName: 'link',
+        firstFormFieldPlaceholder: 'Название',
+        secondFormFieldPlaceholder: 'Ссылка на картинку',
+        buttonName: 'submit',
+        buttonText: '+',
+        path: '/cards'
+    }
+
+    // Окно с формой редактирования профиля
+    const editProfilePopupProps = {
+        id: 'edit-profile',
+        title: 'Редактирование профиля',
+        formName: 'edit',
+        firstFormFieldType: 'text',
+        firstFormFieldName: 'username',
+        secondFieldType: 'text',
+        secondFieldName: 'userdesc',
+        firstFormFieldPlaceholder: 'Имя',
+        secondFormFieldPlaceholder: 'О себе',
+        buttonName: 'submit',
+        buttonText: 'Сохранить',
+        path: '/users/me'
+    }
