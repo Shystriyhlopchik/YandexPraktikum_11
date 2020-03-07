114c3e1880b43eabe58e4607eecc020b2ba32640
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-03-07 20:10:27 +0300

    вынужденный комит

--- webpack.config.js
+++ webpack.config.js
@@ -8,47 +8,113 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 
+// module.exports = {
+//     entry: { main: './src/index.js' },
+//     output: {
+//                 path: path.resolve(__dirname, 'dist'),
+//                 filename: '[name].[chunkhash].js'
+//             },
+//     module: {
+//         rules: [
+//             { 
+//                 test: /\.js$/, 
+//                 use: { loader: "babel-loader" }, 
+//                 exclude: /node_modules/ 
+//             },
+//             {
+//                 test: /\.(eot|ttf|woff|woff2)$/,
+//                 use: 
+//                 [{
+//                     loader: 'file-loader',
+//                     options: { name: 'vendor/[name].[ext]' }
+//                 }]
+//             },
+//             {
+//                 test: /\.(png|jpg|gif|ico|svg)$/,
+//                 use: [
+//                     'file-loader?name=./images/[name].[ext]',
+//                     {
+//                         loader: 'image-webpack-loader',
+//                         options: {
+//                             bypassOnDebug: true,
+//                             disable: true,
+//                             esModule: false
+//                         }
+//                     }
+//                 ]
+//             },
+//             {
+//                 test: /\.css$/,
+//                 use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
+//             }
+//         ]
+//     },
+//     plugins: [
+//         new MiniCssExtractPlugin({
+//             filename: 'style.[contenthash].css',
+//         }),
+//         new OptimizeCssAssetsPlugin({
+//             assetNameRegExp: /\.css$/g,
+//             cssProcessor: require('cssnano'),
+//             cssProcessorPluginOptions: {
+//                     preset: ['default'],
+//             },
+//             canPrint: true
+//         }),
+//         new HtmlWebpackPlugin({ // настроили плагин
+//             inject: false,
+//             template: './src/index.html',
+//             filename: 'index.html'
+//         }),
+//         new WebpackMd5Hash(),
+//         new webpack.DefinePlugin({
+//             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+//         })
+//     ]
+// };
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
-                path: path.resolve(__dirname, 'dist'),
-                filename: '[name].[chunkhash].js'
-            },
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+        },
     module: {
         rules: [
             { 
-                test: /\.js$/, 
-                use: { loader: "babel-loader" }, 
-                exclude: /node_modules/ 
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)$/,
-                use: 
-                [{
-                    loader: 'file-loader',
-                    options: { name: 'vendor/[name].[ext]' }
-                }]
-            },
-            {
-                test: /\.(png|jpg|gif|ico|svg)$/,
-                use: [
+            test: /\.js$/, 
+            use: { loader: "babel-loader" }, 
+            exclude: /node_modules/ 
+                },
+                {
+                    test: /\.(eot|ttf|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'vendor/[name].[ext]'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif|ico|svg)$/,
+                    use: [
                         'file-loader?name=./images/[name].[ext]',
                         {
                             loader: 'image-webpack-loader',
                             options: {
                                 bypassOnDebug: true,
-                                disable: true,
-                                esModule: false
+                                disable: true
                             }
                         }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
-            }
-        ]
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
+                }
+            ]
     },
     plugins: [
         new MiniCssExtractPlugin({
